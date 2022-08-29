c4e4d59557ad78b59117ab623bc490ebf3716ca1
NikitaRekaev <nvvvvk@gmail.com>
2022-08-29 19:31:37 +0200

    Initial Commit

---
+++ SinemaHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SinemaHabits
+//
+//  Created by Nikita Rekaev on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
