da39909fcd4bdb51cb2da05f905db84140c64062
ForestLamp <y10b@ya.ru>
2022-08-30 20:46:45 +0300

    Setup SearchBar

--- SinemaHabits/Modules/MainScreen/Views/MainView.swift
+++ SinemaHabits/Modules/MainScreen/Views/MainView.swift
@@ -54,18 +54,11 @@ class MainView: UIView {
     // MARK: - Public methods
     
     func setupSearchBar() {
-        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
-        textFieldInsideSearchBar?.backgroundColor = UIColor(
-            red: 247.0/255.0,
-            green: 247.0/255.0,
-            blue: 248.0/255.0,
-            alpha: 1)
         
-        searchBar.backgroundColor = .white
-        searchBar.showsBookmarkButton = true
         searchBar.sizeToFit()
         searchBar.placeholder = "Введи название фильма"
         searchBar.setValue("Отмена", forKey: "cancelButtonText")
+        searchBar.showsCancelButton = true
 
     }
 }
