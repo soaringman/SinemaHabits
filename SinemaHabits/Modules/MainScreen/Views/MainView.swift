d2d006acea89898df9b2e1a6ee38feb44b63b9d0
NikitaRekaev <nvvvvk@gmail.com>
2022-08-30 10:13:16 +0200

    deleted Snapkit

--- SinemaHabits/Modules/MainScreen/Views/MainView.swift
+++ SinemaHabits/Modules/MainScreen/Views/MainView.swift
@@ -1,2 +1,21 @@
-import Foundation
-import SnapKit
+import UIKit
+
+class MainView: UIView {
+    
+    let searchBar = UISearchBar()
+    
+    func setupSearchBar() {
+        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideSearchBar?.backgroundColor = UIColor(
+            red: 247.0/255.0,
+            green: 247.0/255.0,
+            blue: 248.0/255.0,
+            alpha: 1)
+        
+        searchBar.backgroundColor = .white
+        searchBar.showsBookmarkButton = true
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Введи название фильма"
+        searchBar.setValue("Отмена", forKey: "cancelButtonText")
+    }
+}
