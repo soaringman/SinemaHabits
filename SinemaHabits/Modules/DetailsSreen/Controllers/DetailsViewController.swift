81726e8316ea894afa06748ed71841ecd6d3a317
Alexey Gulyaev <sargonmeister@gmail.com>
2022-09-02 16:45:49 +0300

    Updated DetailViewController

--- SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
+++ SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
@@ -2,4 +2,12 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
+    private let delailsView = DetailsView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view = delailsView
+    }
+    
 }
