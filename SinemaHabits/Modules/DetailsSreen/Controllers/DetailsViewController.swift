df424401f241ced0e84c9a1eaa55699d08662943
Alexey Gulyaev <sargonmeister@gmail.com>
2022-09-02 20:45:36 +0300

    Updated DetailView

--- SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
+++ SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
@@ -2,12 +2,34 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     
-    private let delailsView = DetailsView()
+    private let detailsView = DetailsView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(false)
         
-        self.view = delailsView
+        detailsView.setupUI()
+        self.view = detailsView
+        dismiss(animated: true)
     }
     
+    func setData(model: FilmAndTVResult) {
+        navigationItem.title = model.name
+        detailsView.image = model.posterPath
+        detailsView.rating = "Rating: \(model.voteAverage)"
+        detailsView.releaseFilmDate = "Date: \(model.firstAirDate ?? "")"
+        detailsView.descriptionFilm = model.overview
+    }
+    
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        print("У нас ничего не работает!!!!")
+        navigationItem.title = ""
+        detailsView.image = ""
+        detailsView.rating = ""
+        detailsView.releaseFilmDate = ""
+        detailsView.descriptionFilm = ""
+    }
 }
