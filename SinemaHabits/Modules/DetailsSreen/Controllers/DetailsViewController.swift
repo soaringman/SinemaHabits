a1484c381bf0ece0727d5213455d105d271defe3
Alexey Gulyaev <sargonmeister@gmail.com>
2022-09-03 22:08:58 +0300

    Refactor back button and format date in russian format date

--- SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
+++ SinemaHabits/Modules/DetailsSreen/Controllers/DetailsViewController.swift
@@ -3,13 +3,15 @@ import UIKit
 class DetailsViewController: UIViewController {
     
     private let detailsView = DetailsView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
     override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(false)
+        navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(
+            title: "",
+            style: .plain,
+            target: self,
+            action: nil)
+        self.navigationController?.navigationBar.tintColor = UIColor(named: "customBlack")
         
         detailsView.setupUI()
         self.view = detailsView
@@ -17,19 +19,32 @@ class DetailsViewController: UIViewController {
     }
     
     func setData(model: FilmAndTVResult) {
-        navigationItem.title = model.name
+        navigationItem.title = model.name 
         detailsView.image = model.posterPath
         detailsView.rating = "Rating: \(model.voteAverage)"
-        detailsView.releaseFilmDate = "Date: \(model.firstAirDate ?? "")"
+        guard let newDate = processDate(string: model.firstAirDate ?? "",
+                                  fromFormat: "yyyy-MM-dd",
+                                        toFormat: "dd.MM.yyyy") else { return }
+        detailsView.releaseFilmDate = "Date: \(newDate)"
         detailsView.descriptionFilm = model.overview
     }
     
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        print("У нас ничего не работает!!!!")
+//        print("У нас ничего не работает!!!!")
         navigationItem.title = ""
         detailsView.image = ""
         detailsView.rating = ""
         detailsView.releaseFilmDate = ""
         detailsView.descriptionFilm = ""
+    }
+    
+    private func processDate(string: String, fromFormat: String = "ddMMyyyy", toFormat: String = "dd MMMM yyyy") -> String? {
+        let formatter = DateFormatter()
+
+        formatter.dateFormat = fromFormat
+        guard let date = formatter.date(from: string) else { return nil }
+
+        formatter.dateFormat = toFormat
+        return formatter.string(from: date)
     }
 }
