a062f3311e65e70022acd6629bcacde9c6835005
NikitaRekaev <nvvvvk@gmail.com>
2022-10-15 13:48:17 +0600

    Refactored code style for MainView

--- SinemaHabits/Sources/Modules/MainScreen/Views/MainView.swift
+++ SinemaHabits/Sources/Modules/MainScreen/Views/MainView.swift
@@ -2,16 +2,20 @@ import UIKit
 
 final class MainView: UIView {
     
+    // MARK: - Properties
+    
     var refresh: (() -> Void)?
     
     // MARK: - UI Elements
     
     let errorView = ErrorView(frame: UIScreen.main.bounds)
+    
+    lazy var tableView: UITableView = UITableView(frame: self.bounds, style: .plain)
+    lazy var refreshControl = UIRefreshControl()
+    lazy var searchController = UISearchController(searchResultsController: nil)
+    lazy var titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 360, height: 40))
+    
     private let searchErrorView = SearchErrorView(frame: UIScreen.main.bounds)
-    public lazy var tableView: UITableView = UITableView(frame: self.bounds, style: .plain)
-    public lazy var refreshControl = UIRefreshControl()
-    public lazy var searchController = UISearchController(searchResultsController: nil)
-    public lazy var titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 360, height: 40))
     
     // MARK: - Initialization
     
@@ -24,35 +28,11 @@ final class MainView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Private methods
-    
-    private func setupConstraints() {
-        addSubview(tableView)
-        tableView.snp.makeConstraints {
-            $0.width.height.equalToSuperview()
-        }
-    }
-    
-    private func setupUI() {
-        
-        tableView.register(MainScreenCell.self, forCellReuseIdentifier: MainScreenCell.reuseID)
-        
-        tableView.refreshControl = refreshControl
-        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
-        
-        searchController.searchBar.searchTextField.clearButtonMode = .never
-        searchController.searchBar.placeholder = "Search cinema..."
-        searchController.searchBar.tintColor = UIColor(named: "customBlack")
-        
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        titleLabel.textAlignment = .left
-        titleLabel.text = "SinemaHabits"
-        
-        errorView.isHidden = true
-        searchErrorView.isHidden = true
-        
-    }
+}
+
+// MARK: - Public Methods
+
+extension MainView {
     
     func setupErrorView() {
         addSubview(errorView)
@@ -73,8 +53,45 @@ final class MainView: UIView {
         tableView.isHidden = false
         tableView.reloadData()
     }
+}
+
+// MARK: - Actions
+
+@objc
+private extension MainView {
     
     @objc private func refreshData() {
         self.refresh?()
+    }
+}
+
+// MARK: - Private methods
+
+private extension MainView {
+    
+    func setupUI() {
+        
+        tableView.register(MainScreenCell.self, forCellReuseIdentifier: MainScreenCell.reuseID)
+        
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        
+        searchController.searchBar.searchTextField.clearButtonMode = .never
+        searchController.searchBar.placeholder = "Search cinema..."
+        searchController.searchBar.tintColor = UIColor(named: "customBlack")
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        titleLabel.textAlignment = .left
+        titleLabel.text = "SinemaHabits"
+        
+        errorView.isHidden = true
+        searchErrorView.isHidden = true
+    }
+    
+    func setupConstraints() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.width.height.equalToSuperview()
+        }
     }
 }
